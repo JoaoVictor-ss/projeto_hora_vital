35138013015e9c362bc94064ac59585e7360e954
Leonardo <leoalvesamc062gmail.com>
2025-11-21 17:10:16 -0300

    cadastro/perfil finalizado

--- src/app/tabs/tabs.page.ts
+++ src/app/tabs/tabs.page.ts
@@ -1,7 +1,13 @@
 import { Component, EnvironmentInjector, inject } from '@angular/core';
-import { IonTabs, IonTabBar, IonTabButton, IonIcon, IonLabel } from '@ionic/angular/standalone';
+import {
+  IonTabs,
+  IonTabBar,
+  IonTabButton,
+  IonIcon,
+  IonLabel,
+} from '@ionic/angular/standalone';
 import { addIcons } from 'ionicons';
-import { addCircle, list, home } from 'ionicons/icons';
+import { addCircle, list, home, personOutline } from 'ionicons/icons';
 
 @Component({
   selector: 'app-tabs',
@@ -13,6 +19,6 @@ export class TabsPage {
   public environmentInjector = inject(EnvironmentInjector);
 
   constructor() {
-    addIcons({ addCircle, list, home });
+    addIcons({ addCircle, list, home, personOutline });
   }
 }
