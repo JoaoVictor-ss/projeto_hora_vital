c193a103911dbf81ae21b0a4b4313d5f2b291c56
leozinr01 <leonardofreitas.ribeiro2@gmail.com>
2025-12-09 18:46:45 -0300

    environ

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,12 @@
 export const environment = {
-  production: true
+  production: true, // <--- Aqui fica TRUE para o APK final
+  firebase: {
+    apiKey: "AIzaSyCD3w3id7EQhwun7fbXtsoWDggeAISkcqw",
+    authDomain: "projeto-29907.firebaseapp.com",
+    projectId: "projeto-29907",
+    storageBucket: "projeto-29907.firebasestorage.app",
+    messagingSenderId: "759920664497",
+    appId: "1:759920664497:web:51cd548c1504a67c0e2790",
+    measurementId: "G-BJ631CJNB6"
+  }
 };
